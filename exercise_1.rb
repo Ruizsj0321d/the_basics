7fb5721397410c6f7e1bded4516bf7e2fa4e5a2b
Steven Ruiz <ruiz.steven.j@gmail.com>
2019-01-27 00:32:04 -0700

    add string concatenation and string interpolation to exercise 1

--- exercise_1.rb
+++ exercise_1.rb
@@ -3,3 +3,17 @@
 puts 'Steven' + 'Ruiz'
 
 puts 'My name is ' + 'Steven ' + 'Jalmire ' 'Ruiz ' + 'Delgado'
+
+# Set up a local variable
+# Use string concatenation to add the two string names together
+
+first_name = 'Steven'
+last_name = 'Ruiz'
+
+puts first_name + last_name
+
+puts first_name + ' ' + last_name
+
+# Use string interpolation now
+
+puts "#{first_name} #{last_name}"
