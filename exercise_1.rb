66ae00d7ae90f5ec2c872ac61870b0e2f01a64ac
Steven Ruiz <ruiz.steven.j@gmail.com>
2019-01-26 20:03:49 -0700

    add first exercise and push my first LS repository

---
+++ exercise_1.rb
@@ -0,0 +1,5 @@
+# add two strings together that, when concatenated, return
+# your first and last name as your full name in one string
+puts 'Steven' + 'Ruiz'
+
+puts 'My name is ' + 'Steven ' + 'Jalmire ' 'Ruiz ' + 'Delgado'
