02166edd239414b7d1ad20f2ae36f786cca523b5
Steven Ruiz <ruiz.steven.j@gmail.com>
2019-01-27 08:42:01 -0700

    done with exercise number 6

---
+++ exercise_6.rb
@@ -0,0 +1,13 @@
+# 6. Write a program that calculates the squares of 3 float numbers
+# of your choosing and outputs the results to the screen.
+
+puts 5.0 ** 2
+puts 2.5 ** 2
+puts 3.75 ** 2
+
+
+# Now I am going to do it in the format the book solution gives.
+
+puts 5.0 * 5.0
+puts 2.5 * 2.5
+puts 3.75 * 3.75
