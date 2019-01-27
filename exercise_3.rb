4be2b1ee5581da57d52c3f3fc5c0bff11865647c
Steven Ruiz <ruiz.steven.j@gmail.com>
2019-01-27 03:09:44 -0700

    this is my third assignment from the_basics chapter

---
+++ exercise_3.rb
@@ -0,0 +1,19 @@
+# Write a program that uses hash to store a list of movie titles
+# with the year they came out.
+# Use puts to make the program print out the year of each movie
+# to the screen so everybody can see and be happy!
+
+movies = {
+  'Aladdin' => '1992',
+  'Aliens' => '1986',
+  'Apocalypse_Now' => '1979',
+  'Conan_the_Barbarian' => '1982',
+  'Upgrade' => '2018'
+}
+
+puts "The year my favorite movies came out:"
+puts movies["Aladdin"]
+puts movies["Aliens"]
+puts movies["Apocalypse_Now"]
+puts movies["Conan_the_Barbarian"]
+puts movies["Upgrade"]
